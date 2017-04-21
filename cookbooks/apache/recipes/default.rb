7266b60f5ba4dc74f408168378aeef7bc6a16ebc
vivek <vivek.velu@asurion.com>
2017-04-21 04:03:51 +0000

    added motd

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,6 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #Install apache package
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :install
@@ -13,4 +20,5 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start,:enable]
 end
+
 
